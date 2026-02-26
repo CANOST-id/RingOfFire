7d00975f10680329f0764846e80373550051686e
CANOST-id <csinner31@gmail.com>
2026-02-26 14:31:35 +0100

    added take card function

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent {
+
+  pickCardAnimation = false;
+
+  constructor() { }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+
+}
