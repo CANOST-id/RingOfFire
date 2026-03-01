c8be9bcded8ca868776205739a8bb004dcd5046e
CANOST-id <csinner31@gmail.com>
2026-03-01 16:27:48 +0100

    added new dialog-add-player component - incl. material dialog

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
+  ],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrls: ['./dialog-add-player.component.scss']
+})
+export class DialogAddPlayerComponent {
+
+  name: string = '';
+
+  constructor() { }
+
+  addPlayer() {
+    console.log(this.name);
+  }
+}
